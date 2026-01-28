6250e51bb6874d0277ee39b8392e4bcef369252c
Vaishnav Babar <vaishnavdbabar@gmail.com>
2026-01-28 21:24:13 +0530

    Add files via upload

---
+++ HappyNumber.java
@@ -0,0 +1,38 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class HappyNumber {
+    public static int sumOfSquares(int n){
+        int sum = 0;
+        while(n>0){
+            int digit = n % 10;
+            sum += digit*digit;
+            n/=10;
+        }
+        return sum;
+    }
+
+    public static boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = sumOfSquares(n);
+        }
+
+        return n == 1;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number : ");
+        int n = sc.nextInt();
+
+        if (isHappy(n)) {
+            System.out.println("The " + n + " is HAPPY 😁 number !!! ");
+        }
+        else {
+            System.out.println("The " + n + " is not ☹️ a happy number ");
+        }
+    }
+}
